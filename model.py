05c3fbdde06b2d7943319089d40cd101ad7ed989
pkseeg <m.parker.seegmiller@gmail.com>
2025-01-13 11:03:59 -0500

    fixed GPU settings

--- model.py
+++ model.py
@@ -1,6 +1,9 @@
+import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def read_olmo():
     olmo_tokenizer = AutoTokenizer.from_pretrained("allenai/OLMo-2-1124-7B-Instruct")
-    olmo_model = AutoModelForCausalLM.from_pretrained("allenai/OLMo-2-1124-7B-Instruct")
+    olmo_model = AutoModelForCausalLM.from_pretrained("allenai/OLMo-2-1124-7B-Instruct").to(device)
     return olmo_model, olmo_tokenizer
