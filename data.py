7fe3a133f4cb5c80b1725da030409464e6655fdd
pkseeg <m.parker.seegmiller@gmail.com>
2025-01-13 10:32:20 -0500

    added example datasets

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-from datasets import load_dataset
+from datasets import load_dataset, load_from_disk
 
 def process_classification(ds):
     def process_example(example):
@@ -9,13 +9,20 @@ def process_classification(ds):
     
     return ds.map(process_example, remove_columns=ds.column_names)
 
-def read_classification_data(a_name, b_name, c_name):
+def read_classification_data(a_name, b_name, c_name, subset_size = 500):
     dataset_list = []
     for name in [a_name, b_name, c_name]:
-        dataset = load_dataset("McAuley-Lab/Amazon-Reviews-2023", name, split="full[:5%]")
+        dataset = load_dataset("McAuley-Lab/Amazon-Reviews-2023", name, split="full")
+        dataset = dataset.shuffle(seed=42).select(range(min(subset_size, len(dataset))))
         processed_ds = process_classification(dataset)
         dataset_list.append(processed_ds)
     return dataset_list
+
+def read_example_data(a_name, b_name, c_name):
+    a = load_from_disk(a_name)
+    b = load_from_disk(b_name)
+    c = load_from_disk(c_name)
+    return a, b, c
 
 
 
@@ -24,3 +31,8 @@ if __name__ == "__main__":
     print(a[0])
     print(b[0])
     print(c[0])
+
+    # Save datasets to disk
+    a.save_to_disk("example_data/kindle_subset")
+    b.save_to_disk("example_data/books_subset")
+    c.save_to_disk("example_data/fashion_subset")
--- semantic_generalizability.py
+++ semantic_generalizability.py
@@ -1,11 +1,14 @@
-from data import read_classification_data
+from data import read_classification_data, read_example_data
 from model import read_olmo 
 from embed import embed
 
 
-def main():
+def main(dev = False):
     # Experiment steps
-    a, b, c = read_classification_data("raw_review_Kindle_Store", "raw_review_Books", "raw_review_Amazon_Fashion")
+    if dev:
+        a, b, c = read_example_data("kindle_store_subset", "books_subset", "amazon_fashion_subset")
+    else:
+        a, b, c = read_classification_data("raw_review_Kindle_Store", "raw_review_Books", "raw_review_Amazon_Fashion")
     olmo_model, olmo_tokenizer = read_olmo()
 
     # 1. measure distance between A, B and A, C using M embedding strategy
@@ -19,4 +22,9 @@ def main():
     # If (across lots of iterations of A, B, and C) 
     # the scores of M_A and B and C correlate with distances between A, B and A, C
     # We can know whether LLMs are able to semantically generalize
+
+
+
+if __name__ == "__main__":
+    main()
 
