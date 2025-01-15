9da54d4bf703c97d2234d94ed66712b98812e33a
pkseeg <m.parker.seegmiller@gmail.com>
2025-01-15 11:48:39 -0500

    updated with info gain

--- embed.py
+++ embed.py
@@ -1,7 +1,8 @@
 import torch
+from tqdm.auto import tqdm
 
 def embed(ds, model, tokenizer):
-    def process_example(example):
+    for example in tqdm(ds):
         print("Embedding a single example")
         text = example["text"]
         print(f"Tokenizing: {text}")
@@ -25,9 +26,35 @@ def embed(ds, model, tokenizer):
         embedding = embedding.cpu()
         # assert False
         return {"embedding": embedding}
+
+    # def process_example(example):
+    #     print("Embedding a single example")
+    #     assert "text" in example
+    #     text = example["text"]
+    #     print(f"Tokenizing: {text}")
+    #     inputs = tokenizer(
+    #         text, 
+    #         padding=True, 
+    #         truncation=True, 
+    #         return_tensors="pt"
+    #     )
+    #     print(f"Put inputs on GPU")
+    #     inputs = {key: value.to(model.device) for key, value in inputs.items()}
+    #     print(f"Getting model outputs")
+    #     with torch.no_grad():
+    #         outputs = model(**inputs, output_hidden_states=True)
+    #     print(f"Accessing hidden state")
+    #     last_hidden_state = outputs.hidden_states[-1]  # Last layer's hidden states
+    #     print(f"Mean pooling")
+    #     embedding = mean_pooling(last_hidden_state, inputs["attention_mask"])
+    #     print(f"Shape of a single embedding: {embedding.shape}")
+    #     #assert False
+    #     embedding = embedding.cpu()
+    #     # assert False
+    #     return {"embedding": embedding}
     
-    print("Embedding dataset.....")
-    return ds.map(process_example, batched=False)
+    #print("Embedding dataset.....")
+    #return ds.map(process_example, batched=False)
 
 def mean_pooling(hidden_states, attention_mask):
     masked_hidden_states = hidden_states * attention_mask.unsqueeze(-1)
